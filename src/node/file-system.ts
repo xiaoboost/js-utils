c3e9903daf8e9639cb4a5f80d5489d630d0b4c88
xiaoboost <xiaoboost@qq.com>
2019-12-12 18:02:29 +0800

    add some file method

--- src/node/file-system.ts
+++ src/node/file-system.ts
@@ -5,11 +5,10 @@ import { promisify } from 'util';
 
 const stat = promisify(fs.stat);
 const readdir = promisify(fs.readdir);
+const remove = promisify(fs.unlink);
 
-/** 求文件夹大小 */
-async function folderSize(base: string) {
-    let result = 0;
-
+/** 遍历文件夹下的所有文件 */
+async function filesOperation(base: string, opt: (file: string, stat: fs.Stats) => void | Promise<void>) {
     const files = await readdir(base);
 
     for (let i = 0; i < files.length; i++) {
@@ -17,18 +16,38 @@ async function folderSize(base: string) {
         const newStat = await stat(newPath);
 
         if (newStat.isDirectory()) {
-            result += await folderSize(newPath);
+            await filesOperation(newPath, opt);
         }
         else {
-            result += newStat.size;
+            await opt(newPath, newStat);
         }
     }
+}
+
+/** 求文件夹大小 */
+async function folderSize(base: string) {
+    let result = 0;
+
+    await filesOperation(base, (_, stat) => {
+        result += stat.size;
+    });
+
+    return result;
+}
+
+/** 获取文件夹内所有文件路径 */
+export async function readdirs(base: string) {
+    const result: string[] = [];
+
+    await filesOperation(base, (file) => {
+        result.push(file);
+    });
 
     return result;
 }
 
 /** 获取文件（夹）大小 */
-export async function getFileSize(base: string) {
+export async function fileSize(base: string) {
     const fileStat = await stat(base);
 
     if (!fileStat.isDirectory()) {
@@ -36,5 +55,17 @@ export async function getFileSize(base: string) {
     }
     else {
         return await folderSize(base);
+    }
+}
+
+/** 删除文件或文件夹 */
+export async function removeFile(base: string) {
+    const fileStat = await stat(base);
+
+    if (!fileStat.isDirectory()) {
+        await filesOperation(base, (file) => remove(file));
+    }
+    else {
+        await remove(base);
     }
 }
