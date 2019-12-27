8fc8ca2511473a1dfa74c3cde69f930a8e64bd32
xiaoboost <xiaoboost@qq.com>
2019-12-27 17:21:48 +0800

    update file system

--- src/node/file-system.ts
+++ src/node/file-system.ts
@@ -3,9 +3,13 @@ import * as path from 'path';
 
 import { promisify } from 'util';
 
-const stat = promisify(fs.stat);
-const readdir = promisify(fs.readdir);
-const remove = promisify(fs.unlink);
+export const readFile = promisify(fs.readFile);
+export const writeFile = promisify(fs.writeFile);
+export const stat = promisify(fs.stat);
+export const readdir = promisify(fs.readdir);
+export const rm = promisify(fs.unlink);
+export const exists = promisify(fs.exists);
+export const mkdir = promisify(fs.mkdir);
 
 /** 遍历文件夹下的所有文件 */
 async function filesOperation(base: string, opt: (file: string, stat: fs.Stats) => void | Promise<void>) {
@@ -35,6 +39,25 @@ async function folderSize(base: string) {
     return result;
 }
 
+/** 读取 json 文件 */
+export async function readJSON<T extends object>(path: string): Promise<T | undefined>;
+export async function readJSON<T extends object>(path: string, initVal: T): Promise<T>;
+export async function readJSON<T extends object>(path: string, initVal?: T) {
+    const content = (await readFile(path).catch(() => '')).toString();
+
+    if (!content) {
+        return initVal;
+    }
+
+    try {
+        return JSON.parse(content) as T;
+    }
+    catch (e) {
+        console.warn(e);
+        return initVal;
+    }
+}
+
 /** 获取文件夹内所有文件路径 */
 export async function readdirs(base: string) {
     const result: string[] = [];
@@ -59,13 +82,30 @@ export async function fileSize(base: string) {
 }
 
 /** 删除文件或文件夹 */
-export async function removeFile(base: string) {
+export async function rmrf(base: string) {
     const fileStat = await stat(base);
 
     if (fileStat.isDirectory()) {
-        await filesOperation(base, (file) => remove(file));
+        await filesOperation(base, (file) => rm(file));
     }
     else {
-        await remove(base);
+        await rm(base);
+    }
+}
+
+/** 依照路径创建文件夹 */
+export async function mkdirp(target: string) {
+    // 待创建的路径
+    const dirs: string[] = [];
+
+    let dir = target;
+
+    while (await exists(dir)) {
+        dirs.push(dir);
+        dir = path.dirname(dir);
+    }
+
+    while (dirs.length > 0) {
+        await mkdir(dirs.shift()!);
     }
 }
